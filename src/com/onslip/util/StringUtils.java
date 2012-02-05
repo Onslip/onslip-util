47cc5fca9b70fb7ef5d72fd5ab148c5f9a8109a2
Martin Blom <martin.blom@onslip.com>
2012-02-05 18:29:34 +0100

    Fixed an array-out-of-bounds exception.

--- src/com/onslip/util/StringUtils.java
+++ src/com/onslip/util/StringUtils.java
@@ -7,15 +7,16 @@ public abstract class StringUtils {
     public static String leftAlign(String str, int len, char c) {
 	char[] buffer = new char[len];
 	Arrays.fill(buffer, c);
-	str.getChars(0, str.length(), buffer, 0);
+        int slen = Math.min(len, str.length());
+	str.getChars(0, slen, buffer, 0);
 	return new String(buffer);
     }
 
     public static String rightAlign(String str, int len, char c) {
 	char[] buffer = new char[len];
 	Arrays.fill(buffer, c);
-	str.getChars(0, str.length(), buffer, len - str.length());
+        int slen = Math.min(len, str.length());
+	str.getChars(0, slen, buffer, len - slen);
 	return new String(buffer);
     }
-
 }
